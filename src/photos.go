2c9cfb11e71f9016c920bd2c6404d385d817e805
Sam Falkner <sam.falkner@gmail.com>
2025-09-25 07:07:47 -0600

    move Photo into photos.go

--- src/photos.go
+++ src/photos.go
@@ -2,6 +2,22 @@ package main
 
 import "log"
 
+type Photo struct {
+	FileSize          int64   `json:"fileSize"`
+	OrderInEntry      int     `json:"orderInEntry"`
+	CreationDevice    string  `json:"creationDevice"`
+	Duration          int     `json:"duration"`
+	Favorite          bool    `json:"favorite"`
+	Type              string  `json:"type"`
+	Identifier        string  `json:"identifier"`
+	Date              string  `json:"date"`
+	ExposureBiasValue float64 `json:"exposureBiasValue"`
+	Height            int     `json:"height"`
+	Width             int     `json:"width"`
+	MD5               string  `json:"md5"`
+	IsSketch          bool    `json:"isSketch"`
+}
+
 type PhotoWallet struct {
 	wallet map[string]*Photo
 }
